6ed826af5c0fb98efa92ba768c2d5f01c4645d04
eunsonny <eunsonnny@gmail.com>
2023-05-07 00:25:22 +0900

    Add: Fira coda 폰트 추가

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,2 +1,5 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import "@fontsource/fira-code";
+
 import "./src/assets/scss/main.scss";
 export { wrapRootElement } from "./internal/gatsby/wrap-root-element";
